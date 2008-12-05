282c3de63cdced7ed455465e1af649534243c49a
Yuval Kogman <nothingmuch@woobling.org>
2008-12-05 12:14:34 +0000

    add version based skip_all to BerkeleyDB::Manager::Test

--- t/lib/BerkeleyDB/Manager/Test.pm
+++ t/lib/BerkeleyDB/Manager/Test.pm
@@ -6,11 +6,27 @@ BerkeleyDB::Manager::Test;
 use strict;
 use warnings;
 
+use BerkeleyDB;
+
 use Test::More;
 
 use base qw(Exporter);
 
 our @EXPORT = qw(sok);
+
+sub import {
+	my ($self, $version, @plan ) = @_;
+
+	if ( $version ) {
+		if ( $BerkeleyDB::db_version < $version ) {
+			plan skip_all => "DB $version required for this test ($BerkeleyDB::db_version available)";
+		} else {
+			plan @plan;
+		}
+	}
+
+	$self->export_to_level(1, $self);
+}
 
 sub sok ($;$) {
 	local $Test::Builder::Level = $Test::Builder::Level + 1;
