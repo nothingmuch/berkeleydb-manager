91ba8fe79d5828514628b6a5bdf6fc1fdd338214
Yuval Kogman <nothingmuch@woobling.org>
2008-12-06 10:45:39 +0000

    version bump

--- lib/BerkeleyDB/Manager.pm
+++ lib/BerkeleyDB/Manager.pm
@@ -17,7 +17,7 @@ use Moose::Util::TypeConstraints;
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = "0.09";
+our $VERSION = "0.10";
 
 use constant HAVE_DB_MULTIVERSION => do { local $@; eval { DB_MULTIVERSION; 1 } };
 
